0d09e4e92326d3c3cf985a0dfff94a89c5f3e577
Ekaterina Tatarnikova <shtilll@mail.ru>
2022-04-27 19:16:12 +0300

    task 2.2

--- 02-javascript-data-types/2-pick/index.js
+++ 02-javascript-data-types/2-pick/index.js
@@ -1,3 +1,4 @@
+//Tatarnikova Ekaterina
 /**
  * pick - Creates an object composed of the picked object properties:
  * @param {object} obj - the source object
@@ -5,5 +6,12 @@
  * @returns {object} - returns the new object
  */
 export const pick = (obj, ...fields) => {
+	
+	let newObj = Object.fromEntries(
+		Object.entries(obj).filter(function(el) { 
+			return fields.includes(el[0]); 			
+		})
+	);
+	return newObj;
 
 };
