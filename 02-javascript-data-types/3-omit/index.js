ab4370297641a1f67d771d55f1881153db913c83
Ekaterina Tatarnikova <shtilll@mail.ru>
2022-04-27 19:25:33 +0300

    task 2.3

--- 02-javascript-data-types/3-omit/index.js
+++ 02-javascript-data-types/3-omit/index.js
@@ -1,3 +1,4 @@
+//Tatarnikova Ekaterina
 /**
  * omit - creates an object composed of enumerable property fields
  * @param {object} obj - the source object
@@ -5,5 +6,12 @@
  * @returns {object} - returns the new object
  */
 export const omit = (obj, ...fields) => {
+	
+	let newObj = Object.fromEntries(
+		Object.entries(obj).filter(function(el) { 
+			return !fields.includes(el[0]); 			
+		})
+	);
+	return newObj;
 
 };
